518d4b5ea1873f7bba414112c0c67c5789c093fd
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2022-08-09 00:31:28 -0700

    feat: add custom background

--- pages/api/ogimage.ts
+++ pages/api/ogimage.ts
@@ -3,6 +3,7 @@ import { withOGImage } from "megasanjay-next-api-og-image";
 interface ImageQueryProps {
   title?: string;
   description?: string;
+  app?: string;
 }
 
 const style = `
@@ -30,7 +31,6 @@ const style = `
     flex-direction: column;
     justify-content: space-between;
     background-size: cover;
-    background-image: url("https://i.imgur.com/vmrHje1.jpg");
     color: white;
   }
   .top {
@@ -65,12 +65,23 @@ const style = `
 
 export default withOGImage<"query", ImageQueryProps>({
   template: {
-    html: ({ title, description }) =>
-      `
-        <div class='main'>
+    html: ({ title, description, app }) => {
+      let topText = "FAIR Data Innovations Hub";
+      let backgroundImage = "hexbackground.jpg";
+
+      if (app === "soda-for-sparc") {
+        topText = `SODA for SPARC Documentation`;
+        backgroundImage = "hexbackground.jpg";
+      } else if (app === "fairshare") {
+        topText = `FAIRshare Documentation`;
+        backgroundImage = "https://i.imgur.com/eMFPXds.png";
+      }
+
+      return `
+        <div class='main' style='background-image: url(https://og-image-seven-eta.vercel.app/${backgroundImage});'>
           <style>${style}</style>  
           <div class='top'>
-            <h1>SODA for SPARC Documentation</h1>
+            <h1>${topText}</h1>
             <h2>${title}</h2>
             <p class='description'>${description}</p>
           </div>
@@ -81,11 +92,12 @@ export default withOGImage<"query", ImageQueryProps>({
             <div class='socials'>
               <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
               <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-              <p className='text-2xl text-white'>/fairdataihub</p>
+              <p className='text-2xl text-white'> | fairdataihub</p>
             </div>
           </div>
         </div>
-      `,
+      `;
+    },
   },
   cacheControl: `public, max-age=604800, immutable`,
   dev: {
